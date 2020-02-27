7409b124ddce74c0a6be03699c90d70890f4037b
Lachlan <lachybubble@gmail.com>
2020-02-27 09:48:01 -0700

    Fixing sizing 1280x800

--- IdeAtePrototype/Form1.Designer.cs
+++ IdeAtePrototype/Form1.Designer.cs
@@ -37,10 +37,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 66);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 151);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(349, 39);
+            this.label1.Size = new System.Drawing.Size(684, 76);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Little Italy";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -48,7 +48,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(252, 245);
+            this.comboBox1.Location = new System.Drawing.Point(539, 401);
             this.comboBox1.MaxDropDownItems = 4;
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(181, 24);
@@ -59,17 +59,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(226, 210);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(493, 366);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(234, 17);
+            this.label2.Size = new System.Drawing.Size(274, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "How many people are dining today?";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(624, 325);
+            this.button1.Location = new System.Drawing.Point(1106, 688);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(144, 53);
             this.button1.TabIndex = 3;
             this.button1.Text = "Next";
             this.button1.UseVisualStyleBackColor = true;
@@ -79,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.ClientSize = new System.Drawing.Size(1262, 753);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
