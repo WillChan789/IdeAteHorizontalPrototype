ef8cf9bb1510b0c59d39bd14e5b453b6c613aeb4
William Chan <35630333+WillChan789@users.noreply.github.com>
2020-02-28 14:28:38 -0700

    new wpf app

--- IdeAtePrototype2/Properties/Settings.Designer.cs
+++ IdeAtePrototype2/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace IdeAtePrototype.Properties
+namespace IdeAtePrototype2.Properties
 {
 
 
